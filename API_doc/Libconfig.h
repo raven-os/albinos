456a9cffa717388395a13eff72debea3d718ddd7
rigaud_b <matthias.rigaud@epitech.eu>
2018-10-02 22:10:41 +0700

    Changed retuned value & add getSetting

--- API_doc/Libconfig.h
+++ API_doc/Libconfig.h
@@ -28,26 +28,28 @@ namespace LibConfig
    */
   struct Subscription;
 
-  void unsubscribe(struct Subscription*);
+  int unsubscribe(struct Subscription*);
 
   /*
    * Config class is used to manipulate configuration
    */
   struct Config;
 
-  struct Id getConfigId(struct Config const *);
+  int getConfigId(struct Config const *, struct Id ** configId);
 
-  void addSetting(struct Config*, char const *name, char const *value); // basic operation to add a new setting
-  void addSettingAlias(struct Config*, char const *name, char const *aliasName); // set alias for given setting
-  void unsetAlias(struct Config*, char const *aliasName);
-  void removeSetting(struct Config*, char const *name);
+  int addSetting(struct Config*, char const *name, char const *value); // basic operation to add a new setting
+  int addSettingAlias(struct Config*, char const *name, char const *aliasName); // set alias for given setting
+  int unsetAlias(struct Config*, char const *aliasName);
+  int removeSetting(struct Config*, char const *name);
 
-  void include(struct Config *config, struct Config const *inheriftFrom); // inherit from another config
+  int getSettingValue(struct Config const*, char const *settingName, char **); //get the setting's value
 
-  struct Subscription subscribeToSetting(struct Config*, char const *name, void *data, FCPTR_ON_CHANGE_NOTIFIER onChange); // be notifier when a setting change
+  int include(struct Config *config, struct Config const *inheritFrom); // inherit from another config
 
-  struct Config createConfig(char const *configName); // create a config with the given name
-  struct Config getConfig(struct Id id); // get config from id
+  int subscribeToSetting(struct Config*, char const *name, void *data, FCPTR_ON_CHANGE_NOTIFIER onChange, struct Subscription **); // be notifier when a setting change
+
+  int createConfig(char const *configName, struct Config **returnedConfig); // create a config with the given name
+  int getConfig(struct Id const* id, struct Config **returnedConfig); // get config from id
 
 #ifdef _cplusplus
 }
