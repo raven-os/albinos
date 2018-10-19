4d521cacde74007183d8c1f237e79dde22487b3c
milerius <roman.sztergbaum@epitech.eu>
2018-10-19 19:50:23 +0200

    setup basis data read event

--- service/service.hpp
+++ service/service.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <string_view>
+#include <iostream>
 #include <filesystem>
 #include <memory>
 #include <uvw.hpp>
@@ -18,18 +20,48 @@ namespace raven
             server_->on<uvw::ErrorEvent>([](auto const &, auto &) { /* TODO: Fill it */ });
             server_->on<uvw::ListenEvent>([](uvw::ListenEvent const &, uvw::PipeHandle &handle) {
                 std::shared_ptr<uvw::PipeHandle> socket = handle.loop().resource<uvw::PipeHandle>();
-                socket->once<uvw::CloseEvent>(
-                    [&handle](uvw::CloseEvent const &, uvw::PipeHandle &) { handle.close(); });
+                socket->on<uvw::CloseEvent>(
+                    [&handle](uvw::CloseEvent const &, uvw::PipeHandle &) {
+                        std::cout << "close event receive" << std::endl;
+                        handle.close();
+                    });
+                socket->on<uvw::EndEvent>([](const uvw::EndEvent &, uvw::PipeHandle &sock) {
+                    std::cout << "end event receive" << std::endl;
+                    sock.close();
+                });
 
-                //! TODO: do more
+                socket->on<uvw::DataEvent>([](const uvw::DataEvent &data, uvw::PipeHandle &sock) {
+                    std::string_view data_str(data.data.get(), data.length);
+                    std::cout << "data received: " << data_str;
+
+                    //TODO: Treat the data here
+
+                });
+
+                handle.accept(*socket);
+                std::cout << "socket connected" << std::endl;
+                socket->read();
             });
         }
 
         void run() noexcept
         {
-            server_->bind((std::filesystem::temp_directory_path() / "raven-os_service_libconfig.sock").string());
+            clean_socket();
+            std::string socket = (std::filesystem::temp_directory_path() / "raven-os_service_libconfig.sock").string();
+            std::cout << "binding to socket: " << socket << std::endl;
+            server_->bind(socket);
             server_->listen();
             uv_loop_->run();
+        }
+
+        //! In case that the service have been stopped, we want to remove the old socket and create a new one.
+        void clean_socket() noexcept
+        {
+            auto socket_path = std::filesystem::temp_directory_path() / "raven-os_service_libconfig.sock";
+            if (std::filesystem::exists(socket_path)) {
+                std::cout << "socket: " << socket_path.string() << " already exist, removing" << std::endl;
+                std::filesystem::remove(socket_path);
+            }
         }
 
     private:
