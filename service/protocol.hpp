202be63d148a0edd928a0e5cebbd9084ed5c408f
Sztergbaum Roman <roman.sztergbaum@epitech.eu>
2018-12-21 22:46:18 +0100

    [#22]: API capabilities implementation for service (#27)
    [Work on #22]: API capabilities implementation for service
           - add sqlite_modern_cpp and update CMakeLists.txt
           - Rework CONFIG_CREATE request
           - add first json_recipes (@matthiasrigaud can use this recipes for documentation
           - add CONFIG_LOAD request
           - add json_recipe for CONFIG_LOAD

--- service/protocol.hpp
+++ service/protocol.hpp
@@ -11,16 +11,38 @@ namespace raven
 {
   namespace json = nlohmann;
 
+  //! Keywords
+  static inline constexpr const char *request_keyword = "REQUEST_NAME";
+
+  //! Protocol Constants
+  static inline constexpr const char *config_name_keyword = "CONFIG_NAME";
+  static inline constexpr const char *config_key_keyword = "CONFIG_KEY";
+  static inline constexpr const char *config_read_only_key_keyword = "READONLY_CONFIG_KEY";
+
+  //! CONFIG_CREATE
   struct config_create
   {
-    const std::string order{"CONFIG_CREATE"};
     std::string config_name;
-    std::string provider;
   };
 
   inline void from_json(const raven::json::json &json_data, config_create &cfg)
   {
-    cfg.config_name = json_data.at("config").get<std::string>();
-    cfg.provider = json_data.at("provider").get<std::string>();
+      cfg.config_name = json_data.at(config_name_keyword).get<std::string>();
+  }
+
+  //! CONFIG_LOAD
+  struct config_load
+  {
+    std::optional<std::string> config_key{std::nullopt};
+    std::optional<std::string> config_read_only_key{std::nullopt};
+  };
+
+  inline void from_json(const raven::json::json &json_data, config_load &cfg)
+  {
+      if (json_data.count(config_key_keyword) > 0) {
+          cfg.config_key = json_data.at(config_key_keyword).get<std::string>();
+      } else if (json_data.count(config_read_only_key_keyword) > 0) {
+          cfg.config_read_only_key = json_data.at(config_read_only_key_keyword).get<std::string>();
+      }
   }
 }
--- service/service.hpp
+++ service/service.hpp
@@ -17,71 +17,93 @@ namespace raven
 {
   class service
   {
+  private:
+    template <typename Request>
+    Request fill_request(json::json &json_data)
+    {
+        Request request;
+        from_json(json_data, request);
+        return request;
+    }
+
   public:
     void create_config(json::json &json_data)
     {
-      config_create cfg;
-      from_json(json_data, cfg);
-      std::cout << "json receive:\n" << std::setw(4) << json_data << std::endl;
-      std::cout << "cfg.config_name: " << cfg.config_name << std::endl;
-      std::cout << "cfg.provider: " << cfg.provider << std::endl;
+        auto cfg = fill_request<config_create>(json_data);
+        std::cout << "json receive:\n" << std::setw(4) << json_data << std::endl;
+        std::cout << "cfg.config_name: " << cfg.config_name << std::endl;
+        // TODO: Insert in SQL
+    }
+
+    void load_config(json::json &json_data)
+    {
+        auto cfg = fill_request<config_load>(json_data);
+        std::cout << "json receive:\n" << std::setw(4) << json_data << std::endl;
+        if (cfg.config_key) {
+            std::cout << "cfg.config_key: " << cfg.config_key.value() << std::endl;
+        }
+        if (cfg.config_read_only_key) {
+            std::cout << "cfg.config_read_only_key: " << cfg.config_read_only_key.value() << std::endl;
+        }
     }
 
     service()
     {
-      server_->on<uvw::ErrorEvent>([](auto const &, auto &) { /* TODO: Fill it */ });
-      server_->on<uvw::ListenEvent>([this](uvw::ListenEvent const &, uvw::PipeHandle &handle) {
-        std::shared_ptr<uvw::PipeHandle> socket = handle.loop().resource<uvw::PipeHandle>();
-        socket->on<uvw::CloseEvent>([](uvw::CloseEvent const &, uvw::PipeHandle &) { std::cout << "socket closed." << std::endl; });
+        server_->on<uvw::ErrorEvent>([](auto const &, auto &) { /* TODO: Fill it */ });
+        server_->on<uvw::ListenEvent>([this](uvw::ListenEvent const &, uvw::PipeHandle &handle) {
+            std::shared_ptr<uvw::PipeHandle> socket = handle.loop().resource<uvw::PipeHandle>();
+            socket->on<uvw::CloseEvent>(
+                [](uvw::CloseEvent const &, uvw::PipeHandle &) { std::cout << "socket closed." << std::endl; });
 
-        socket->on<uvw::EndEvent>([](const uvw::EndEvent &, uvw::PipeHandle &sock) {
-          std::cout << "end event received" << std::endl;
-          sock.close();
+            socket->on<uvw::EndEvent>([](const uvw::EndEvent &, uvw::PipeHandle &sock) {
+                std::cout << "end event received" << std::endl;
+                sock.close();
+            });
+
+            socket->on<uvw::DataEvent>([this](const uvw::DataEvent &data, uvw::PipeHandle &sock) {
+                static const std::unordered_map<std::string, std::function<void(json::json &)>>
+                    order_registry{{"CONFIG_CREATE", [this](json::json &json_data) {
+                    this->create_config(json_data);
+                }},
+                                   {"CONFIG_LOAD",   [this](json::json &json_data) {
+                                       this->load_config(json_data);
+                                   }},
+                                   {"CONFIG_UNLOAD", [this](json::json &json_data) { /* TODO: fill it */ }}};
+
+                std::string_view data_str(data.data.get(), data.length);
+                try {
+                    auto json_data = json::json::parse(data_str);
+                    std::string command_order = json_data.at(raven::request_keyword).get<std::string>();
+                    order_registry.at(command_order)(json_data);
+                }
+                catch (const json::json::exception &error) {
+                    std::cerr << "error in received data: " << error.what() << std::endl;
+                }
+            });
+
+            handle.accept(*socket);
+            std::cout << "socket connected" << std::endl;
+            socket->read();
         });
-
-        socket->on<uvw::DataEvent>([this](const uvw::DataEvent &data, uvw::PipeHandle &sock) {
-          static const std::unordered_map<std::string, std::function<void(json::json &)>>
-            order_registry{{"CONFIG_CREATE", [this](json::json &json_data) { this->create_config(json_data); }},
-                           {"CONFIG_LOAD", [this](json::json &json_data) { /* TODO: fill it */ }},
-                           {"CONFIG_UNLOAD", [this](json::json &json_data) { /* TODO: fill it */ }}};
-
-          std::string_view data_str(data.data.get(), data.length);
-          try
-            {
-              auto json_data = json::json::parse(data_str);
-              std::string command_order = json_data.at("order").get<std::string>();
-              order_registry.at(command_order)(json_data);
-            }
-          catch (const json::json::exception &error)
-            {
-              std::cerr << "error in received data: " << error.what() << std::endl;
-            }
-        });
-
-        handle.accept(*socket);
-        std::cout << "socket connected" << std::endl;
-        socket->read();
-      });
     }
 
     void run() noexcept
     {
-      clean_socket();
-      std::string socket = (std::filesystem::temp_directory_path() / "raven-os_service_libconfig.sock").string();
-      std::cout << "binding to socket: " << socket << std::endl;
-      server_->bind(socket);
-      server_->listen();
-      uv_loop_->run();
+        clean_socket();
+        std::string socket = (std::filesystem::temp_directory_path() / "raven-os_service_libconfig.sock").string();
+        std::cout << "binding to socket: " << socket << std::endl;
+        server_->bind(socket);
+        server_->listen();
+        uv_loop_->run();
     }
 
     //! In case that the service have been stopped, we want to remove the old socket and create a new one.
     void clean_socket() noexcept
     {
-      auto socket_path = std::filesystem::temp_directory_path() / "raven-os_service_libconfig.sock";
-      if (std::filesystem::exists(socket_path))
-        {
-          std::cout << "socket: " << socket_path.string() << " already exist, removing" << std::endl;
-          std::filesystem::remove(socket_path);
+        auto socket_path = std::filesystem::temp_directory_path() / "raven-os_service_libconfig.sock";
+        if (std::filesystem::exists(socket_path)) {
+            std::cout << "socket: " << socket_path.string() << " already exist, removing" << std::endl;
+            std::filesystem::remove(socket_path);
         }
     }
 
