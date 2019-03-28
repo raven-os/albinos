f539989f72b3287f40b77a1f101f724cbdee0b8f
Arthur Cros <cros_b@etna-alternance.net>
2019-03-28 08:33:22 +0100

    Temporary workaround for id lookup

--- service/client.hpp
+++ service/client.hpp
@@ -20,17 +20,20 @@ namespace raven
 
     client() = delete;
 
-    client &operator+=(raven::config_id_st id)
+    client &operator+=(raven::config_id_st db_id)
     {
         last_id++;
-        config_ids_.insert({{last_id.value(), id.value()}});
+        config_ids_.insert({{last_id.value(), db_id.value()}});
+        reverse_config_ids_.insert({{db_id.value(), last_id.value()}});
         return *this;
     }
 
     client &operator-=(raven::config_id_st id)
     {
         DLOG_F(INFO, "erasing id: %lu from config: %d", id.value(), static_cast<int>(this->sock_->fileno()));
+        auto db_id = config_ids_.at(id.value());
         config_ids_.erase(id.value());
+        reverse_config_ids_.erase(db_id);
         return *this;
     }
 
@@ -43,5 +46,6 @@ namespace raven
     client_ptr sock_;
     raven::config_id_st last_id{0};
     std::unordered_map<raven::config_id_st::value_type, raven::config_id_st::value_type> config_ids_;
+    std::unordered_map<raven::config_id_st::value_type, raven::config_id_st::value_type> reverse_config_ids_; // temporary workaround for a basic id lookup, will need in the future to be able to do that outside of the client class
   };
 };
