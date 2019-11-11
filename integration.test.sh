3b6c6cc0b1445bc0ea332df4514f222a6a038e9b
Roman Sztergbaum <rmscastle@gmail.com>
2019-11-11 09:21:32 +0100

    wip: start integration test

---
+++ integration.test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+config_key=""
+function test_config_creation() {
+    echo `cat API_doc/json_recipes/config_create.json` | nc -U /tmp/raven-os_service_albinos.sock -q1 > res.json
+    request_state=`jq -r '.REQUEST_STATE' res.json`
+    config_key=`jq -r '.CONFIG_KEY' res.json`
+    [[ "${request_state}" == "SUCCESS" ]] && echo "Success" || echo "Failure"
+    rm res.json
+}
+
+function test_config_load() {
+    echo "{\"REQUEST_NAME\": \"CONFIG_LOAD\", \"CONFIG_KEY\": \"${config_key}\"}" | nc -U /tmp/raven-os_service_albinos.sock -q1 > res.json
+    request_state=`jq -r '.REQUEST_STATE' res.json`
+    [[ "${request_state}" == "SUCCESS" ]] && echo "Success" || echo "Failure"
+    rm res.json
+}
+
+test_config_creation
+test_config_load
+
