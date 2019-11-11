f752c893140d2cc9cd340deff9f2bf45c7cf3e83
Roman Sztergbaum <rmscastle@gmail.com>
2019-11-11 09:29:08 +0100

    wip: add config unloading

--- integration.test.sh
+++ integration.test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 config_key=""
+config_id=""
 function test_config_creation() {
     echo "testing config creation"
     echo `cat API_doc/json_recipes/config_create.json` | nc -U /tmp/raven-os_service_albinos.sock -q1 > res.json
@@ -15,6 +16,17 @@ function test_config_load() {
     echo "testing config loading"
     echo "{\"REQUEST_NAME\": \"CONFIG_LOAD\", \"CONFIG_KEY\": \"${config_key}\"}" | nc -U /tmp/raven-os_service_albinos.sock -q1 > res.json
     request_state=`jq -r '.REQUEST_STATE' res.json`
+    config_id=`jq -r '.CONFIG_ID' res.json`
+    echo ${config_id}
+    echo "api output: " `cat res.json`
+    [[ "${request_state}" == "SUCCESS" ]] && echo "Success" || echo "Failure"
+    rm res.json
+}
+
+function test_config_unload() {
+    echo "testing config unloading"
+    echo "{\"REQUEST_NAME\": \"CONFIG_UNLOAD\", \"CONFIG_ID\": ${config_id}}" | nc -U /tmp/raven-os_service_albinos.sock -q1 > res.json
+    request_state=`jq -r '.REQUEST_STATE' res.json`
     echo "api output: " `cat res.json`
     [[ "${request_state}" == "SUCCESS" ]] && echo "Success" || echo "Failure"
     rm res.json
@@ -22,4 +34,5 @@ function test_config_load() {
 
 test_config_creation
 test_config_load
+test_config_unload
 
