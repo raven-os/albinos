34208b4c7de0772329007211b7a5f607f757905f
Roman Sztergbaum <rmscastle@gmail.com>
2019-11-23 08:46:02 +0100

    wip: add api get key and get readonly tests

--- lib/albinos.integ.tests.cpp
+++ lib/albinos.integ.tests.cpp
@@ -14,8 +14,23 @@ TEST_CASE("All case")
     Albinos::Config albinos_cfg = Albinos::Config("foo");
     Albinos::Config* ptr = &albinos_cfg;
 
-    //! Creation
+    //! Creation config
     CHECK_EQ(Albinos::createConfig("toto", &ptr), Albinos::SUCCESS);
 
+    //! Get key
+    Albinos::Key key;
+    CHECK_EQ(Albinos::getConfigKey(ptr, &key), Albinos::SUCCESS);
+    std::string str("key is: [ " + std::string(key.data) + " ]");
+    MESSAGE(str);
+    CHECK(not std::string(key.data).empty());
+
+    //! Readonly key
+    Albinos::Key readonly_key;
+    CHECK_EQ(Albinos::getReadOnlyConfigKey(ptr, &readonly_key), Albinos::SUCCESS);
+    std::string readonly_key_str("key is: [ " + std::string(readonly_key.data) + " ]");
+    MESSAGE(readonly_key_str);
+    CHECK(not std::string(readonly_key.data).empty());
+
+    //! Destruction config
     CHECK_EQ(Albinos::destroyConfig(ptr), Albinos::SUCCESS);
 }
