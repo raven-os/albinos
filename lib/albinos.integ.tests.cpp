38e0d99f0291094035822b4a7c64f687b0aadfb7
Roman Sztergbaum <rmscastle@gmail.com>
2019-11-24 07:56:40 +0100

    wip: add check for key size

--- lib/albinos.integ.tests.cpp
+++ lib/albinos.integ.tests.cpp
@@ -23,6 +23,7 @@ TEST_CASE("All case")
     std::string str("key is: [ " + std::string(key.data) + " ]");
     MESSAGE(str);
     CHECK(not std::string(key.data).empty());
+    CHECK_GT(key.size, 0);
 
     //! Readonly key
     Albinos::Key readonly_key;
@@ -30,6 +31,7 @@ TEST_CASE("All case")
     std::string readonly_key_str("key is: [ " + std::string(readonly_key.data) + " ]");
     MESSAGE(readonly_key_str);
     CHECK(not std::string(readonly_key.data).empty());
+    CHECK_GT(readonly_key.size, 0);
 
     //! Destruction config
     CHECK_EQ(Albinos::destroyConfig(ptr), Albinos::SUCCESS);
