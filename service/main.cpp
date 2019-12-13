02b15a80cafffe5accd533e1eb29a79c6b2d81e6
Arthur Cros <cros_b@etna-alternance.net>
2019-12-13 04:32:11 +0100

    add file logging

--- service/main.cpp
+++ service/main.cpp
@@ -1,8 +1,11 @@
 #include <filesystem>
+#include <loguru.hpp>
 #include "service.hpp"
 
 int main()
 {
+  loguru::add_file("/var/log/albinos.log", loguru::Append, loguru::Verbosity_INFO);
+  loguru::add_file("/var/log/latest_albinos.log", loguru::Truncate, loguru::Verbosity_MAX);
   raven::service service;
   service.run();
   return 0;
